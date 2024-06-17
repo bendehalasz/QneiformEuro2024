904b46f42e882a85f9fd04160bf3ee3976013809
bendehalasz <he6u9m@inf.elte.hu>
2024-06-17 14:35:44 +0200

    add link to team

--- script.js
+++ script.js
@@ -44,11 +44,17 @@ function checkCredentials() {
             document.getElementById('drawn_person_name').innerHTML = countries[profile_index]
 
             
-            const countryCode = euro2024Teams[countries[profile_index]];
+            const countryElement = euro2024Teams[countries[profile_index]];
+
             const flagElement = document.createElement('span');
-            flagElement.className = `flag-icon flag-icon-${countryCode}`;
+            flagElement.className = `flag-icon flag-icon-${countryElement.code}`;
             flagElement.id = 'country_flag';
             document.getElementById('drawn_person').appendChild(flagElement);
+
+            const urlElement = document.createElement('span');
+            urlElement.id = "country_url";
+            urlElement.innerHTML = `To learn more about your team, click <a href="https://www.uefa.com/european-qualifiers/teams/${countryElement.url}/" target=blank>here</a>`;
+            document.getElementById('drawn_person').appendChild(urlElement);
 
             document.getElementById('drawn_person').classList.remove('hidden');
             document.getElementById('drawn_person').classList.add('show');
@@ -132,31 +138,32 @@ var passwords = ['pitch91', 'own goal73', 'net19', 'tournament75', 'tournament10
 var countries =  ['Denmark', 'Serbia', 'Poland', 'Turkey', 'Albania', 'Switzerland', 'Romania', 'Austria', 'Slovakia', 'Croatia', 'Georgia', 'Czech Republic', 'Netherlands', 'Hungary', 'England', 'Italy', 'Belgium', 'Germany', 'Spain', 'Ukraine', 'Scotland']
 
 const euro2024Teams = {
-    "Albania": "al",
-    "Austria": "at",
-    "Belgium": "be",
-    "Croatia": "hr",
-    "Czech Republic": "cz",
-    "Denmark": "dk",
-    "England": "gb",
-    "France": "fr",
-    "Georgia": "ge",
-    "Germany": "de",
-    "Hungary": "hu",
-    "Italy": "it",
-    "Netherlands": "nl",
-    "Poland": "pl",
-    "Portugal": "pt",
-    "Romania": "ro",
-    "Scotland": "gb-sct",
-    "Serbia": "rs",
-    "Slovakia": "sk",
-    "Slovenia": "si",
-    "Spain": "es",
-    "Switzerland": "ch",
-    "Turkey": "tr",
-    "Ukraine": "ua"
+    "Albania": { code: "al", url: "2--albania" },
+    "Austria": { code: "at", url: "8--austria" },
+    "Belgium": { code: "be", url: "13--belgium" },
+    "Croatia": { code: "hr", url: "56370--croatia" },
+    "Czech Republic": { code: "cz", url: "58837--czechia" },
+    "Denmark": { code: "dk", url: "35--denmark" },
+    "England": { code: "gb", url: "39--england" },
+    "France": { code: "fr", url: "43--france" },
+    "Georgia": { code: "ge", url: "57157--georgia" },
+    "Germany": { code: "de", url: "47--germany" },
+    "Hungary": { code: "hu", url: "57--hungary" },
+    "Italy": { code: "it", url: "66--italy" },
+    "Netherlands": { code: "nl", url: "95--netherlands" },
+    "Poland": { code: "pl", url: "109--poland" },
+    "Portugal": { code: "pt", url: "110--portugal" },
+    "Romania": { code: "ro", url: "113--romania" },
+    "Scotland": { code: "gb-sct", url: "117--scotland" },
+    "Serbia": { code: "rs", url: "147--serbia" },
+    "Slovakia": { code: "sk", url: "58836--slovakia" },
+    "Slovenia": { code: "si", url: "57163--slovenia" },
+    "Spain": { code: "es", url: "122--spain" },
+    "Switzerland": { code: "ch", url: "128--switzerland" },
+    "Turkey": { code: "tr", url: "135--turkiye" },
+    "Ukraine": { code: "ua", url: "57166--ukraine" }
 };
+
 
 
 const count = 200,
