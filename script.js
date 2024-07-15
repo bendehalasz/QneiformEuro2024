42f6e4f5a1a49c2c34e0c6f22634fd1961412074
bendehalasz <he6u9m@inf.elte.hu>
2024-07-15 09:47:01 +0200

    fix goal differnece

--- script.js
+++ script.js
@@ -3,30 +3,30 @@ const passwords = ['nutmeg29', 'pass28', 'referee53', 'goal35', 'hat-trick28', '
 var countries = ['Spain', 'Portugal', 'Ukraine', 'Albania', 'Denmark', 'Belgium', 'Italy', 'Croatia', 'Romania', 'Hungary', 'Germany', 'France', 'Serbia', 'England', 'Netherlands', 'Turkey', 'Czech Republic', 'Georgia', 'Scotland', 'Switzerland', 'Austria', 'Slovenia', 'Slovakia', 'Poland'];
 
 const euro2024Teams = {
-    "Albania": { code: "al", url: "2--albania", rank: "Top 24", cards: "7", worst: "-1" },
-    "Austria": { code: "at", url: "8--austria", rank: "Top 16", cards: "12", worst: "-1" },
-    "Belgium": { code: "be", url: "13--belgium", rank: "Top 16", cards: "8", worst: "-1" },
+    "Albania": { code: "al", url: "2--albania", rank: "Top 24", cards: "7", worst: "-2" },
+    "Austria": { code: "at", url: "8--austria", rank: "Top 16", cards: "12", worst: "1" },
+    "Belgium": { code: "be", url: "13--belgium", rank: "Top 16", cards: "8", worst: "0" },
     "Croatia": { code: "hr", url: "56370--croatia", rank: "Top 24", cards: "7", worst: "-3" },
-    "Czech Republic": { code: "cz", url: "58837--czechia", rank: "Top 24", cards: "15", worst: "-1" },
+    "Czech Republic": { code: "cz", url: "58837--czechia", rank: "Top 24", cards: "15", worst: "-2" },
     "Denmark": { code: "dk", url: "35--denmark", rank: "Top 16", cards: "9", worst: "-2" },
-    "England": { code: "gb-eng", url: "39--england", rank: "2", cards: "14", worst: "-1" },
-    "France": { code: "fr", url: "43--france", rank: "Top 4", cards: "9", worst: "-1" },
+    "England": { code: "gb-eng", url: "39--england", rank: "2", cards: "14", worst: "2" },
+    "France": { code: "fr", url: "43--france", rank: "Top 4", cards: "9", worst: "1" },
     "Georgia": { code: "ge", url: "57157--georgia", rank: "Top 16", cards: "7", worst: "-3" },
-    "Germany": { code: "de", url: "47--germany", rank: "Top 8", cards: "14", worst: "-1" },
-    "Hungary": { code: "hu", url: "57--hungary", rank: "Top 24", cards: "10", worst: "-2" },
+    "Germany": { code: "de", url: "47--germany", rank: "Top 8", cards: "14", worst: "7" },
+    "Hungary": { code: "hu", url: "57--hungary", rank: "Top 24", cards: "10", worst: "-3" },
     "Italy": { code: "it", url: "66--italy", rank: "Top 16", cards: "10", worst: "-2" },
-    "Netherlands": { code: "nl", url: "95--netherlands", rank: "Top 4", cards: "11", worst: "-1" },
-    "Poland": { code: "pl", url: "109--poland", rank: "Top 24", cards: "8", worst: "-2" },
-    "Portugal": { code: "pt", url: "110--portugal", rank: "Top 8", cards: "5", worst: "-2" },
-    "Romania": { code: "ro", url: "113--romania", rank: "Top 16", cards: "9", worst: "-3" },
-    "Scotland": { code: "gb-sct", url: "117--scotland", rank: "Top 24", cards: "6", worst: "-4" },
+    "Netherlands": { code: "nl", url: "95--netherlands", rank: "Top 4", cards: "11", worst: "3" },
+    "Poland": { code: "pl", url: "109--poland", rank: "Top 24", cards: "8", worst: "-3" },
+    "Portugal": { code: "pt", url: "110--portugal", rank: "Top 8", cards: "5", worst: "2" },
+    "Romania": { code: "ro", url: "113--romania", rank: "Top 16", cards: "9", worst: "-2" },
+    "Scotland": { code: "gb-sct", url: "117--scotland", rank: "Top 24", cards: "6", worst: "-5" },
     "Serbia": { code: "rs", url: "147--serbia", rank: "Top 24", cards: "9", worst: "-1" },
     "Slovakia": { code: "sk", url: "58836--slovakia", rank: "Top 16", cards: "8", worst: "-1" },
     "Slovenia": { code: "si", url: "57163--slovenia", rank: "Top 16", cards: "12", worst: "0" },
-    "Spain": { code: "es", url: "122--spain", rank: "1", cards: "17", worst: "1" },
-    "Switzerland": { code: "ch", url: "128--switzerland", rank: "Top 8", cards: "10", worst: "0" },
-    "Turkey": { code: "tr", url: "135--turkiye", rank: "Top 8", cards: "21", worst: "-3" },
-    "Ukraine": { code: "ua", url: "57166--ukraine", rank: "Top 24", cards: "3", worst: "-3" }
+    "Spain": { code: "es", url: "122--spain", rank: "1", cards: "17", worst: "11" },
+    "Switzerland": { code: "ch", url: "128--switzerland", rank: "Top 8", cards: "10", worst: "4" },
+    "Turkey": { code: "tr", url: "135--turkiye", rank: "Top 8", cards: "21", worst: "0" },
+    "Ukraine": { code: "ua", url: "57166--ukraine", rank: "Top 24", cards: "3", worst: "-2" }
 };
 
 const tbody = document.querySelector('tbody');
